b3379625116992cc249cec22ba87cb1b16d8f5d2
josh <josh.ribakoff@gmail.com>
2013-06-30 12:42:21 -0400

    swap order of date/time

--- application/Default/views/helpers/Datetime.php
+++ application/Default/views/helpers/Datetime.php
@@ -3,6 +3,6 @@ class Zend_View_Helper_Datetime
 {
     public function datetime($date, $time = null)
     {
-        return date('h:ia n/j/Y', strtotime($date . ' ' . $time));
+        return date('n/j/Y h:ia', strtotime($date . ' ' . $time));
     }
 }
